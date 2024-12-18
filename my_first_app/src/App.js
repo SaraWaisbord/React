f9deb5170a315d9bc926ee49e2d2a9b420eb4d87
SaraWaisbord <s0504119750@gmail.com>
2024-12-18 18:03:47 +0200

    lesson3 - useEffect and conditional rendering

--- my_first_app/src/App.js
+++ my_first_app/src/App.js
@@ -1,40 +1,19 @@
 
 import { useState } from 'react';
-import ImgComponent from "../src/lesson2/components/ImgComponent"
+// import ImgComponent from "../src/lesson2/components/ImgComponent"
 
-import a from '../src/lesson2/assats/a.png'
-import b from '../src/lesson2/assats/b.png'
-import c from '../src/lesson2/assats/c.png'
-import d from '../src/lesson2/assats/d.png'
-import e from '../src/lesson2/assats/e.png'
-import f from '../src/lesson2/assats/f.png'
+// import a from '../src/lesson2/assats/a.png'
+// import b from '../src/lesson2/assats/b.png'
+// import c from '../src/lesson2/assats/c.png'
+// import d from '../src/lesson2/assats/d.png'
+// import e from '../src/lesson2/assats/e.png'
+// import f from '../src/lesson2/assats/f.png'
+import WeatherComponent from './components/WeatherComponent';
 function App() {
-
-  const [imgs, setImg] = useState([a, b, c, d, e, f])
-  const [idx, setIndex] = useState(0)
-
-  function checkNext(e) {
-    console.log(idx);
-    if (idx == 5) {
-      return true;
-    }
-    return false;
-  }
-  function checkBack(e) {
-    if (idx == 0) {
-      return true;
-    }
-    return false;
-  }
+      // <WeatherComponent></WeatherComponent>
   return (
     <>
-      <button disabled={checkNext()} onClick={(e) => {
-        setIndex(idx + 1)
-      }}> next </button>
-      <ImgComponent path={imgs[idx]}></ImgComponent>
-      <button disabled={checkBack()} onClick={(e) => { setIndex(idx - 1) }
-      }
-      > back </button>
+        <WeatherComponent></WeatherComponent>
     </>
   );
 }
