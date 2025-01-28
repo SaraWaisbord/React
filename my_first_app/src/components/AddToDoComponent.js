10cdd2899efbc26f8c4d7b40c6929e9163e35c03
SaraWaisbord <s0504119750@gmail.com>
2025-01-28 17:48:25 +0200

    adding DB, update for c#

--- my_first_app/src/components/AddToDoComponent.js
+++ my_first_app/src/components/AddToDoComponent.js
@@ -1,42 +1,31 @@
-
 import { useDispatch , useSelector} from "react-redux";
 import {addToDoAction, countIdAction} from '../Redux/actions'
 import { useState } from "react";
 
-const AddToDoComponent = ()=>{
+const AddToDoComponent = () => {
     const [start,setStart] = useState('');
     const [finish,setFinish] = useState('');
     const [name,setName] = useState('');
     const [priority,setPriority] = useState('');
     const id = useSelector(state => state.id);
     const dispatch = useDispatch();
-//הוספתי משתנה לסטייט - יופיע נכון בלוג נוסיף את זה כמזהה הID של המשימה ואם לא....🤔
-    console.log(id);
     return (
         <>
         <form>
         <h2 className="label-wrapper">
           <label>What needs to be done?</label>
         </h2>
-        <input type="text" id="new-todo-name"  placeholder="name"    onBlur={(e) => setName(e.target.value)} />
-        <input type="text" id="new-todo-start"    placeholder="start"  onBlur={(e) => setStart(e.target.value)}/>
-        <input type="text" id="new-todo-finish"   placeholder="finish" onBlur={(e) => setFinish(e.target.value)}/>
-        <input type="text" id="new-todo-priority" placeholder="priority"  onBlur={(e) => setPriority(e.target.value)}/>
+        <input type="text" id="new-todo-name"  placeholder="name"           onBlur={(e) => setName(e.target.value)} />
+        <input type="number" id="new-todo-start"    placeholder="start"     onBlur={(e) => setStart(e.target.value)}/>
+        <input type="number" id="new-todo-finish"   placeholder="finish"    onBlur={(e) => setFinish(e.target.value)}/>
+        <input type="number" id="new-todo-priority" placeholder="priority"  onBlur={(e) => setPriority(e.target.value)}/>
         </form>
         <>
-        {/* אני שמה את הפרמטרים פה כדי לבדוק אם הוא מצליח בכלל לקרוא משדה הקלט */}
-        {/* {priority}{id}{finish}{start} */}
-        {/* מצליח! הבעיה לא פה... */}
-        
-        <button type="submit" onClick={()=>dispatch(addToDoAction({start:start,finish:finish,name:name,priority:priority,id:id}),dispatch(countIdAction()))}>
+        <button type="submit" onClick={()=>(dispatch(addToDoAction({start:start,finish:finish,name:name,priority:priority,id:id}),dispatch(countIdAction())))}>
             Add😵
         </button>
-
         </>
-
         </>
-
     )
-   
 }
 export default AddToDoComponent;
